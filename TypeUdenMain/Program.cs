74e21da821008b283010732612bafd17f1dbc0a1
Lpopp30 <l.poppmadsen@gmail.com>
2026-04-15 08:28:23 +0200

    Add project files.

---
+++ TypeUdenMain/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using TypeUdenMain;
+using System.IO;
+
+Console.WriteLine("Skriv et ord:");
+string Word = Console.ReadLine();
+Console.WriteLine("Skriv en tekst:");
+string Text = Console.ReadLine();
+
+Console.WriteLine($"Ordet '{Word}' forekommer {WordCount.CountWords(Word, Text)} gange i teksten.");
+bool? ResultBool = WordCount.TrueOrFalse(WordCount.CountWords(Word, Text), Word, Text);
+
+if  (ResultBool == null)
+{
+    Console.WriteLine("Det søgte ord findes ikke i det angivet tekst");
+}
+else if (ResultBool == false)
+{
+    Console.WriteLine($"Ordet '{Word}' forkommer {WordCount.CountWords(Word, Text)} gange");
+}
+else
+{
+    Console.WriteLine($"Det søgte ord '{Word}' findes {WordCount.CountWords(Word, Text)} gange i det angivet tekst");
+    
+}
---
+++ TypeUdenMain/WordCount.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TypeUdenMain
+{
+    internal class WordCount
+    {
+        public static int CountWords(string word, string text)
+        {
+            if (string.IsNullOrEmpty(word) || string.IsNullOrEmpty(text))
+                return 0;
+
+            word = word.ToLower();
+            text = text.ToLower();
+
+            int count = 0;
+            for (int i = 0; i + word.Length <= text.Length; i++)
+            {
+                int j = 0;
+                while (j < word.Length && text[i + j] == word[j]) j++;
+                if (j == word.Length) count++;
+            }
+
+            return count;
+        }
+
+        public static bool? TrueOrFalse(int count, string word, string Text)
+        {
+            if (count == 0)
+                return null;
+            
+            else if (count >= 1 && count <= 10)
+                return false;
+            
+            else
+                File.WriteAllText("C:\\Users\\lpopp\\filename.txt", Text);
+            return true;
+            
+        }
+    }
+}
